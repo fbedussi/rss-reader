c3918b5c142328a3c51a577e652a8d13f7fb52f7
Francesco Bedussi <francesco@francescobedussi.it>
2018-03-11 21:56:54 +0100

    fix https errors?

--- src/GetFeeds.elm
+++ src/GetFeeds.elm
@@ -15,7 +15,7 @@ getFeeds sites =
 getSiteFeed : Site -> Task String (List Article)
 getSiteFeed site =
     Http.get
-        ("https://api.rss2json.com/v1/api.json?rss_url=" ++ encodeUri site.rssLink)
+        ("//api.rss2json.com/v1/api.json?rss_url=" ++ encodeUri site.rssLink)
         (feedDecoder site.id)
         |> Http.toTask
         |> Task.mapError (\err -> "Error reading feeds for site: " ++ site.name)
